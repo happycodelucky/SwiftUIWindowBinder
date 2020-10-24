b9bd8aded81afe6d1e713b9a1a99b7fb8f25b902
happycodelucky <paul.a.bates@gmail.com>
2020-10-24 15:24:05 -0700

    Adding new sections

---
+++ Playgrounds/WindowBinderPlayground.playbook.playground/Pages/GotchasOfWindowBinder.xcplaygroundpage/Contents.swift
@@ -0,0 +1,66 @@
+/*:
+ # WindowBinder - What Not to Do!
+
+ Attempting to access the `@State var window` property from within the *construction* of a view, that is not inside
+ an escaping closure, will cause SwiftUI to track state changes on `window`.
+
+ This seems ideal but a host window
+ object will not be availble until the underlying `UIView` or `NSView` is in the actual view hierarchy.
+
+ To see this in action keep reading. But first, run the playground to see what happens
+ */
+import SwiftUI
+import SwiftUIWindowBinder
+/*:
+ ## Conditionals
+ Change the conditionals here to change the playground execution
+ */
+struct Conditions {
+    /// When set to `true`, a window @State property will be used in view construction.
+    ///
+    /// First run with `false` and later, when instructed, change to `true`
+    static let useWindow = true
+}
+/*:
+ ## Content view
+ Our playground's live content view
+ */
+struct ContentView : View {
+    //: Use a state property to capture the `Window` from `WindowBinder` (below)
+    // Use a state property to capture the Window from WindowBinder (below)
+    @State var window: Window?
+
+    /// View body
+    var body: some View {
+        // Add some logging so we can see what's going on
+        print("Building ContentView...")
+        if (Conditions.useWindow) {
+            // Here we are using `self.window` so it needs to be under the conditional
+            print("> window=\(window?.debugDescription ?? "nil")")
+        }
+
+        return WindowBinder(window: $window) {
+            // The use of `self.window` during view construction informs SwiftUI to track `window` changes,
+            // resulting in multiple calls until SwiftUI bails
+
+            // Trying to make the text be based on `window` state (don't do this!)
+            if Conditions.useWindow {
+                Text(window?.description ?? "nil")
+                    .font(.title)
+                    .padding()
+            } else {
+                Text("Hello")
+                    .font(.title)
+                    .padding()
+            }
+
+
+        }
+    }
+}
+
+
+
+// Present the view controller in the Live View window
+import PlaygroundSupport
+PlaygroundPage.current.setLiveView(ContentView())
---
+++ Playgrounds/WindowBinderPlayground.playbook.playground/Pages/WrappingUp.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+/*:
+ # Wrapping Up
+
+ The two major take aways from the `SwiftUIWindowBinder` package is `WindowButton` and `WindowBinder`. They both operate alike
+ with different encapsulation.
+
+ ## What, No View Modifiers?
+ No, I'm not being lazy here. I did write a few [View Modifiers](https://developer.apple.com/documentation/swiftui/viewmodifier),
+ stopped myself, and removed them. `WindowButton` was all the convenience I wanted to expose and I even thought if I should do
+ that.
+
+ There was a few objections:
+ 1. Lots of boilerplate code, and maintence here
+ 2. Overloading futureproofing (if Apple decides to overload `onTapGesture`'s `action:` to receive something else
+ 3. Most important - providing a `Window` to buttons or event oriented view modifiers isn't actual "The Right Way" (see next section)
+
+ But, if you really must, here's the code for overloading the `onTapGesture` (now optionally receives a `Window`).
+ Repeat for all modifiers you want to support.
+ */
+ import SwiftUI
+ import SwiftUIWindowBinder
+
+ @available(tvOS, unavailable)
+ struct WindowBindableOnTap: ViewModifier {
+    @State var window: Window?
+
+    var count: Int
+    var action: (Window) -> Void
+
+    func body(content: Content) -> some View {
+        WindowBinder(window: $window) {
+            content.onTapGesture(count: count) {
+                guard let window = self.window else {
+                    return
+                }
+
+                action(window)
+            }
+        }
+    }
+ }
+
+ @available(tvOS, unavailable)
+ extension View {
+    /// Adds an action to perform when this view recognizes a tap gesture.
+    public func onTapGesture(count: Int = 1, perform action: @escaping (Window) -> Void) -> some View {
+        return self.modifier(WindowBindableOnTap(count: count, action: action))
+    }
+ }
+/*:
+ ## The Right Way
+ `WindowButton` is a convenience, it gets you where you need to get to. Eventually you code will replace `WindowButton` `with
+ `Button` as SwiftUI matures and new native SwiftUI functions become available.
+
+ What I mean by "The Right Way" is you code should be using a wrapper, if possible, not accessing a `UIWindow`/`NSWindow`
+ directly. For me personally, and why this package was created, `ASWebAuthenticationSession` would have a wrapper much like
+ `Alert` is for SwiftUI. The right way, is instead to create a SwiftUI wrapper for `ASWebAuthenticationSession` using
+ `WindowBinder` and not use it directly.
+
+ ## Enjoy!
+ SwiftUI is evolving, getting a ton of new features each release. This package represents a bit of a polyfill assistance
+ (hello JavaScript + Bable nomenclature) until the time when we have official wrappers for the things we want.
+
+ I don't see UIKit or AppKit disappearing from beneath SiwftUI anytime soon (likely never), and a few of us will continue
+ to find a need for such things like this package.
+
+ Of course, bugs, issues, pull requests, corrections, or comments fire away
+
+ Paul.
+ */
